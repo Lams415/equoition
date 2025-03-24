99455c4433de47fe239327faf4d056e46a07f5cf
Lams415 <amsellemeyal93100@gmail.com>
2025-03-24 16:07:08 +0200

    declaration de variable

---
+++ script.js
@@ -0,0 +1,17 @@
+
+console.log("calculatrice eyal")
+
+var X =  10;
+var Y = 20;
+var addition = X + Y;
+var soustraction = X - Y;
+var multiplication = X * Y;
+var division = X / Y;
+var separation = "------------------------------";
+var tableau = [12,25,38,47,];
+console.log(addition)
+console.log(soustraction)
+console.log(multiplication)
+console.log(division)
+console.log(separation)
+console.log(tableau[3] + tableau[1])
